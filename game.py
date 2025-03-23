d2f11f0f74aecbf810583bb840fd66a7f24d0cbf
Piotr Świeży <piotrmichalswiezy@gmail.com>
2025-03-23 13:58:32 +0100

    Initial commit

---
+++ game.py
@@ -0,0 +1,19 @@
+import pygame
+from pygame.examples.moveit import WIDTH
+
+pygame.init()
+
+CUBE_SIZE = 25
+CUBES_NUM = 20
+WIDTH = CUBE_SIZE *CUBES_NUM
+skreen = pygame.display.set_mode((WIDTH, WIDTH))
+WHITE  = (255, 255, 255)
+BLUE = (0, 0, 255)
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Use a breakpoint in the code line below to debug your script.
+ # Press Ctrl+F8 to toggle the breakpoint.
+# Press the green button in the gutter to run the script.
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
